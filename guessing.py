734b4aeb6211f9eb60de7105345d702d6208a43a
Kayiranga <donjesuskayiranga@gmail.com>
2026-02-27 22:11:25 +0200

    changing the guessable number

--- guessing.py
+++ guessing.py
@@ -1,18 +1,15 @@
-number = 67
-
+number = 78
 attempt = 0
-
 for i in range(5):
      guess = float(input("Guess the number:"))
      if guess > number:  
            print("Too high") 
      elif guess < number:
-            print("Too low")       
+            print("Too low")     
      attempt += 1
      if  number == guess:
        print(f"correct! You got it in {attempt} attempts")
        break
      if attempt == 5:
-
       print(f"Game Over! The number was {number}")
      
