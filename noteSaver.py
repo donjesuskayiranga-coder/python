aaab913b1c816d9e59719187c564a73b34e4a6db
Kayiranga <donjesuskayiranga@gmail.com>
2026-03-17 21:37:42 +0200

    simple changes

---
+++ noteSaver.py
@@ -0,0 +1,16 @@
+def save_note(note):
+    with open("notes.txt", "a") as file:
+        file.write(note + "\n")
+        print("Note saved!")
+def read_notes():
+    with open("notes.txt", "r") as file:
+        for line in file:print(line.strip()) 
+while True:
+    choice = input("1. Add note 2. Read notes 3. Quit: ")
+    if choice == "1":
+        note = input("Enter your note: ")
+        save_note(note)
+    elif choice == "2":
+        read_notes()
+    elif choice == "3":
+        break       
