48657c7e5686b9aff9405796481901c4c8ef09aa
Kayiranga <donjesuskayiranga@gmail.com>
2026-03-17 21:44:33 +0200

    simple changes

---
+++ what.py
@@ -0,0 +1,11 @@
+import re
+
+data = open("image.jpg", "rb").read()
+
+# find all long digit sequences ≥ 50 digits
+nums = re.findall(rb"[0-9]{50,}", data)
+
+for i, n in enumerate(nums[:10], 1):
+    print(f"[{i}] length={len(n)} digits")
+    print(n.decode(errors="ignore"))
+    print()
