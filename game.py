3f0cdadeda9fa38779d37f0c28c9978b99c6c615
Kayiranga <donjesuskayiranga@gmail.com>
2026-03-25 21:12:49 +0200

    its guessing a secret from 1 to 100 until you get it correct using random module

---
+++ game.py
@@ -0,0 +1,24 @@
+import random
+
+def guessing_game():
+    secret = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        try:
+            guess = int(input("Enter a number(1-100): "))
+            attempts += 1
+
+            if guess > secret:
+                print("too high")
+            elif guess < secret:
+                print("too low")    
+            else:
+                print(f"Correct! you got it in {attempts} attempts!")
+                break
+
+        except ValueError:
+            print("Enter a valid number !") 
+
+guessing_game()                       
+
