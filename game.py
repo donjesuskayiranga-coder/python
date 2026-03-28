55c091bdef0af24a2c68e3efee4a57de9ff5ee7a
Kayiranga <donjesuskayiranga@gmail.com>
2026-03-28 11:22:55 +0200

    changing the range of secret!

--- game.py
+++ game.py
@@ -3,10 +3,9 @@ import random
 def guessing_game():
     secret = random.randint(1, 100)
     attempts = 0
-
     while True:
         try:
-            guess = int(input("Enter a number(1-100): "))
+            guess = int(input("Enter a number(1-50): "))
             attempts += 1
             if guess > secret:
                 print("too high")
